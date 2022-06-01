9a2b106476a8afd9b16ae889a5f9500f32f35e2a
Una865 <62244939+Una865@users.noreply.github.com>
2022-06-01 23:11:49 +0200

    Update detect_single_threaded.py

--- detect_single_threaded.py
+++ detect_single_threaded.py
@@ -79,7 +79,9 @@ if __name__ == '__main__':
     #cv2.namedWindow('Single-Threaded Detection', cv2.WINDOW_NORMAL)
     image_np = cv2.imread(args.image_source)
     print(args.image_source)
-    cv2.imshow('Single-Threaded Detection',image_np)
+    path = args.image_source.split('/')[-1]
+    dir = "/content/runs/" +path
+    cv2.imwrite('Single-Threaded Detection',image_np)
     #im_height,im_width,im_channels  = image_np.shape
     
     #boxes, scores = detector_utils.detect_objects(image_np,
