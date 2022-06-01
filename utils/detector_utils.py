41a0a66cd847f752d9d435ce32e8611e30d4a281
Una865 <62244939+Una865@users.noreply.github.com>
2022-06-01 23:54:28 +0200

    Update detector_utils.py

--- utils/detector_utils.py
+++ utils/detector_utils.py
@@ -51,14 +51,20 @@ def load_inference_graph():
 
 # draw the detected bounding boxes on the images
 # You can modify this to also draw a label.
-def draw_box_on_image(num_hands_detect, score_thresh, scores, boxes, im_width, im_height, image_np):
+def draw_box_on_image(num_hands_detect, score_thresh, scores, boxes, im_width, im_height, image_np,path):
+    list_of_lines = []
     for i in range(num_hands_detect):
         if (scores[i] > score_thresh):
             (left, right, top, bottom) = (boxes[i][1] * im_width, boxes[i][3] * im_width,
                                           boxes[i][0] * im_height, boxes[i][2] * im_height)
+            one_line = str(int(left))+" "+ str(int(top))+" "+str(int(right))+" "+str(int(bottom)))
+            list_of_lines.append(one_line)
             p1 = (int(left), int(top))
             p2 = (int(right), int(bottom))
             cv2.rectangle(image_np, p1, p2, (77, 255, 9), 3, 1)
+    f = open(path,"w")
+    f.writelines(list_of_lines)
+    f.close()
 
 
 # Show fps value on image.
