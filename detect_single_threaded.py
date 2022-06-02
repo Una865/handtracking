2bb9af04a37f59c3b668101bd03aba52cc70aa42
Una865 <62244939+Una865@users.noreply.github.com>
2022-06-02 10:51:00 +0200

    Update detect_single_threaded.py

--- detect_single_threaded.py
+++ detect_single_threaded.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
         '-src',
         '--source',
         #dest='image_source',
-        dest = 'video_source,
+        dest = 'video_source',
         default=0,
         help='Device index of the camera.')
     parser.add_argument(
