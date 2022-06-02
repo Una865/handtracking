b40cb590356cfd9b16b9182dfad1ca7f3aa456e7
Una865 <62244939+Una865@users.noreply.github.com>
2022-06-02 10:55:35 +0200

    Update detect_single_threaded.py

--- detect_single_threaded.py
+++ detect_single_threaded.py
@@ -26,8 +26,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '-src',
         '--source',
-        #dest='image_source',
-        dest = 'video_source',
+        dest='image_source',
         default=0,
         help='Device index of the camera.')
     parser.add_argument(
@@ -75,7 +74,7 @@ if __name__ == '__main__':
     #num_frames = 0
     #im_width, im_height = (cap.get(3), cap.get(4))
     # max number of hands we want to detect/track
-    '''num_hands_detect = 2
+    num_hands_detect = 2
 
     #cv2.namedWindow('Single-Threaded Detection', cv2.WINDOW_NORMAL)
     image_np = cv2.imread(args.image_source)
@@ -94,60 +93,7 @@ if __name__ == '__main__':
                                          scores, boxes, im_width, im_height,
                                          image_np,text_file)
     
-    cv2.imwrite(dir,image_np)'''
+    cv2.imwrite(dir,image_np)
 
     
-    cap = cv2.VideoCapture(args.video_source)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, args.width)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, args.height)
-
-    start_time = datetime.datetime.now()
-    num_frames = 0
-    im_width, im_height = (cap.get(3), cap.get(4))
-    # max number of hands we want to detect/track
-    num_hands_detect = 2
-
-    cv2.namedWindow('Single-Threaded Detection', cv2.WINDOW_NORMAL)
-
-    while True:
-        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-        ret, image_np = cap.read()
-        # image_np = cv2.flip(image_np, 1)
-        try:
-            image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
-        except:
-            print("Error converting to RGB")
-
-        # Actual detection. Variable boxes contains the bounding box cordinates for hands detected,
-        # while scores contains the confidence for each of these boxes.
-        # Hint: If len(boxes) > 1 , you may assume you have found atleast one hand (within your score threshold)
-
-        boxes, scores = detector_utils.detect_objects(image_np,
-                                                      detection_graph, sess)
-
-        # draw bounding boxes on frame
-        detector_utils.draw_box_on_image(num_hands_detect, args.score_thresh,
-                                         scores, boxes, im_width, im_height,
-                                         image_np)
-
-        # Calculate Frames per second (FPS)
-        num_frames += 1
-        elapsed_time = (datetime.datetime.now() - start_time).total_seconds()
-        fps = num_frames / elapsed_time
-
-        if (args.display > 0):
-            # Display FPS on frame
-            if (args.fps > 0):
-                detector_utils.draw_fps_on_image("FPS : " + str(int(fps)),
-                                                 image_np)
-
-            cv2.imshow('Single-Threaded Detection',
-                       cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR))
-
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                cv2.destroyAllWindows()
-                break
-        else:
-            print("frames processed: ", num_frames, "elapsed time: ",
-                  elapsed_time, "fps: ", str(int(fps)))
-  
+    
