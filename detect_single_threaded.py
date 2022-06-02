f60c1c3573e192cafc7449295d644e7ea3f5a2a8
Una865 <62244939+Una865@users.noreply.github.com>
2022-06-02 10:59:09 +0200

    Update detect_single_threaded.py

--- detect_single_threaded.py
+++ detect_single_threaded.py
@@ -78,6 +78,7 @@ if __name__ == '__main__':
 
     #cv2.namedWindow('Single-Threaded Detection', cv2.WINDOW_NORMAL)
     image_np = cv2.imread(args.image_source)
+    cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
     
     path = args.image_source.split('/')[-1]
     print(path)
